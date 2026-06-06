df7b84c9b6e760106fdd5799c29468749725b4ee
JHW0407 <jhw0407@hs.ac.kr>
2026-06-07 06:14:29 +0900

    Create questions.js

---
+++ src/data/questions.js
@@ -0,0 +1,45 @@
+const questions = [
+
+  {
+    question:'친구들과 있을 때 에너지가 충전된다',
+    type:'E'
+  },
+
+  {
+    question:'혼자만의 시간이 꼭 필요하다',
+    type:'I'
+  },
+
+  {
+    question:'상상과 아이디어를 자주 떠올린다',
+    type:'N'
+  },
+
+  {
+    question:'현실적인 정보를 더 믿는다',
+    type:'S'
+  },
+
+  {
+    question:'결정할 때 논리를 우선한다',
+    type:'T'
+  },
+
+  {
+    question:'다른 사람 감정을 중요하게 생각한다',
+    type:'F'
+  },
+
+  {
+    question:'계획이 정리되어 있어야 마음이 편하다',
+    type:'J'
+  },
+
+  {
+    question:'즉흥적인 상황이 재미있다',
+    type:'P'
+  }
+
+]
+
+export default questions
