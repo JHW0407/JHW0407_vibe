404a25fa398ee5a68d746960f40c0066acc1dceb
JHW0407 <jhw0407@hs.ac.kr>
2026-06-07 06:22:15 +0900

    Create results.js

---
+++ src/data/results.js
@@ -0,0 +1,25 @@
+const results = {
+
+  INFP: {
+    title:'달빛 몽상가',
+    desc:'감성이 풍부하며 이상적인 세상을 꿈꾸는 타입'
+  },
+
+  ENFP: {
+    title:'자유로운 불꽃',
+    desc:'열정과 에너지로 주변 분위기를 밝게 만드는 타입'
+  },
+
+  INTJ: {
+    title:'고독한 전략가',
+    desc:'차분하게 미래를 설계하는 분석형 타입'
+  },
+
+  ESTJ: {
+    title:'카리스마 리더',
+    desc:'현실적이며 조직을 이끄는 능력이 뛰어난 타입'
+  }
+
+}
+
+export default results
