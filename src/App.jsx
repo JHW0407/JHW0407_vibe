17c8c198bd612266be69d66d0c50ef5bb6543682
JHW0407 <jhw0407@hs.ac.kr>
2026-06-08 22:36:40 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,70 @@
 import { useState } from 'react'
-import Start from './components/Start'
-import Question from './components/Question'
-import Result from './components/Result'
+import Intro from './components/Intro'
+import QuestionCard from './components/QuestionCard'
+import ResultCard from './components/ResultCard'
+import Progress from './components/Progress'
+import Footer from './components/Footer'
 
 import questions from './data/questions'
+import results from './data/results'
 
 function App() {
-
   const [started, setStarted] = useState(false)
   const [current, setCurrent] = useState(0)
-
   const [scores, setScores] = useState({
-    E:0,I:0,S:0,N:0,T:0,F:0,J:0,P:0
+    E: 0,
+    I: 0,
+    S: 0,
+    N: 0,
+    T: 0,
+    F: 0,
+    J: 0,
+    P: 0,
   })
 
   const [finished, setFinished] = useState(false)
+  const [mbti, setMbti] = useState('')
 
-  const answerQuestion = (type, value) => {
-
-    setScores(prev => ({
-      ...prev,
-      [type]: prev[type] + value
-    }))
-
-    if(current + 1 < questions.length){
-      setCurrent(current + 1)
+  const handleAnswer = (type) => {
+    const updated = {
+      ...scores,
+      [type]: scores[type] + 1,
     }
-    else{
+
+    setScores(updated)
+
+    if (current + 1 < questions.length) {
+      setCurrent(current + 1)
+    } else {
+      const result =
+        (updated.E > updated.I ? 'E' : 'I') +
+        (updated.S > updated.N ? 'S' : 'N') +
+        (updated.T > updated.F ? 'T' : 'F') +
+        (updated.J > updated.P ? 'J' : 'P')
+
+      setMbti(result)
       setFinished(true)
     }
   }
 
-  const getResult = () => {
+  if (!started) {
+    return <Intro onStart={() => setStarted(true)} />
+  }
 
-    let result = ''
-
-    result += scores.E >= scores.I ? 'E' : 'I'
-    result += scores.S >= scores.N ? 'S' : 'N'
-    result += scores.T >= scores.F ? 'T' : 'F'
-    result += scores.J >= scores.P ? 'J' : 'P'
-
-    return result
+  if (finished) {
+    return <ResultCard result={results[mbti]} />
   }
 
   return (
     <div className="app">
+      <Progress current={current + 1} total={questions.length} />
 
-      {!started && (
-        <Start onStart={() => setStarted(true)} />
-      )}
+      <QuestionCard
+        question={questions[current]}
+        onAnswer={handleAnswer}
+      />
 
-      {started && !finished && (
-        <Question
-          data={questions[current]}
-          current={current}
-          total={questions.length}
-          onAnswer={answerQuestion}
-        />
-      )}
-
-      {finished && (
-        <Result type={getResult()} />
-      )}
-
+      <Footer />
     </div>
   )
 }
