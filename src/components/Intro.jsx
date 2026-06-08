58358b9ca55d4880d9fed32774dfe57a0b393465
JHW0407 <jhw0407@hs.ac.kr>
2026-06-08 22:46:29 +0900

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,28 +1,30 @@
+import Footer from "./Footer";
+
 function Intro({ onStart }) {
   return (
-    <div
-      className="intro"
-      style={{
-        backgroundImage: "url('/images/hero-bg.jpg')",
-      }}
-    >
-      <div className="overlay">
-        <img src="/images/logo.png" className="logo" />
+    <div className="intro">
+      <img
+        src="/images/logo.png"
+        alt="Auracast"
+        className="main-logo"
+      />
 
-        <div className="title-box">
-          ✨🌌💫
-          <h1>Auracast</h1>
-          💜🪐⭐
-        </div>
-
-        <p className="subtitle">
-          당신의 오라와 성격을 분석해보세요
-        </p>
-
-        <button onClick={onStart}>테스트 시작하기</button>
+      <div className="title-box">
+        ✨🌈 Auracast 🌈✨
       </div>
+
+      <p className="intro-text">
+        당신의 오라와 성격을 분석하는
+        감성 MBTI 테스트
+      </p>
+
+      <button className="start-btn" onClick={onStart}>
+        테스트 시작하기
+      </button>
+
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Intro
+export default Intro;
