3e11f6ea5f666f19b881b034eb0d7f38d4346339
JHW0407 <jhw0407@hs.ac.kr>
2026-06-08 22:46:42 +0900

    Update QuestionCard.jsx

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,19 +1,23 @@
+import "../styles/question.css";
+
 function QuestionCard({ question, onAnswer }) {
   return (
     <div className="question-card">
       <h2>{question.question}</h2>
 
-      <div className="btn-group">
-        <button onClick={() => onAnswer(question.a.type)}>
-          {question.a.text}
-        </button>
+      <button
+        onClick={() => onAnswer(question.a.type)}
+      >
+        {question.a.text}
+      </button>
 
-        <button onClick={() => onAnswer(question.b.type)}>
-          {question.b.text}
-        </button>
-      </div>
+      <button
+        onClick={() => onAnswer(question.b.type)}
+      >
+        {question.b.text}
+      </button>
     </div>
-  )
+  );
 }
 
-export default QuestionCard
+export default QuestionCard;
