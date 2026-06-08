6c7778a05b30bdd7ffed9b06c86550b8bbbd8a8e
JHW0407 <jhw0407@hs.ac.kr>
2026-06-08 22:47:12 +0900

    Update Progress.jsx

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,14 +1,14 @@
 function Progress({ current, total }) {
-  const percent = (current / total) * 100
+  const percent = (current / total) * 100;
 
   return (
     <div className="progress-wrap">
       <div
         className="progress-bar"
         style={{ width: `${percent}%` }}
-      />
+      ></div>
     </div>
-  )
+  );
 }
 
-export default Progress
+export default Progress;
