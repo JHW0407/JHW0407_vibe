f740f01e1b226a3cbf7c61c77aa8b7399fe8a01c
JHW0407 <jhw0407@hs.ac.kr>
2026-06-08 23:35:41 +0900

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ function Intro({ onStart }) {
   return (
     <div className="intro">
       <img
-        src="/images/logo.png"
+        src="images/logo.png"
         alt="Auracast"
         className="main-logo"
       />
@@ -15,10 +15,14 @@ function Intro({ onStart }) {
 
       <p className="intro-text">
         당신의 오라와 성격을 분석하는
+        <br />
         감성 MBTI 테스트
       </p>
 
-      <button className="start-btn" onClick={onStart}>
+      <button
+        className="start-btn"
+        onClick={onStart}
+      >
         테스트 시작하기
       </button>
 
