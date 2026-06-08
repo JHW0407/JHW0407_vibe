ae04a09dfed15c9bdd35d2a7309f98b75b83dc45
JHW0407 <jhw0407@hs.ac.kr>
2026-06-08 22:46:54 +0900

    Update ResultCard.jsx

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,15 +1,27 @@
+import "../styles/result.css";
+
 function ResultCard({ result }) {
   return (
     <div className="result-card">
-      <img src={result.image} className="result-img" />
-
       <h1>{result.mbti}</h1>
+
+      <img
+        src={result.image}
+        alt={result.mbti}
+        className="result-image"
+      />
 
       <h2>{result.title}</h2>
 
-      <p>{result.description}</p>
+      <p>{result.desc}</p>
+
+      <button
+        onClick={() => window.location.reload()}
+      >
+        다시 테스트하기
+      </button>
     </div>
-  )
+  );
 }
 
-export default ResultCard
+export default ResultCard;
