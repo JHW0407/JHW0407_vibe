e4f3b6cdc6b8502f907fdab1865381ec401fa602
JHW0407 <jhw0407@hs.ac.kr>
2026-06-08 23:44:05 +0900

    Update results.js

--- src/data/results.js
+++ src/data/results.js
@@ -3,112 +3,112 @@ const results = {
     mbti: "INFP",
     title: "감성 몽상가",
     desc: "이상과 감성을 중요하게 생각하는 타입",
-    image: "images/result/infp.png",
+    image: "./images/result/infp.png",
   },
 
   ENFP: {
     mbti: "ENFP",
     title: "열정적인 활동가",
     desc: "밝고 창의적인 분위기 메이커",
-    image: "images/result/enfp.png",
+    image: "./images/result/enfp.png",
   },
 
   INTJ: {
     mbti: "INTJ",
     title: "전략가",
     desc: "계획적이고 분석적인 성격",
-    image: "images/result/intj.png",
+    image: "./images/result/intj.png",
   },
 
   ESTJ: {
     mbti: "ESTJ",
     title: "통솔자",
     desc: "체계적이고 책임감이 강함",
-    image: "images/result/estj.png",
+    image: "./images/result/estj.png",
   },
 
   INFJ: {
     mbti: "INFJ",
     title: "통찰자",
     desc: "깊은 공감 능력을 가진 이상주의자",
-    image: "images/result/infj.png",
+    image: "./images/result/infj.png",
   },
 
   INTP: {
     mbti: "INTP",
     title: "논리술사",
     desc: "호기심 많고 분석적인 성격",
-    image: "images/result/intp.png",
+    image: "./images/result/intp.png",
   },
 
   ENFJ: {
     mbti: "ENFJ",
     title: "정의로운 리더",
     desc: "따뜻하고 사람을 이끄는 타입",
-    image: "images/result/enfj.png",
+    image: "./images/result/enfj.png",
   },
 
   ENTP: {
     mbti: "ENTP",
     title: "토론가",
     desc: "창의적이고 아이디어가 풍부함",
-    image: "images/result/entp.png",
+    image: "./images/result/entp.png",
   },
 
   ENTJ: {
     mbti: "ENTJ",
     title: "지도자",
     desc: "목표지향적이고 카리스마 있음",
-    image: "images/result/entj.png",
+    image: "./images/result/entj.png",
   },
 
   ESTP: {
     mbti: "ESTP",
     title: "모험가",
     desc: "에너지 넘치고 활동적임",
-    image: "images/result/estp.png",
+    image: "./images/result/estp.png",
   },
 
   ESFJ: {
     mbti: "ESFJ",
     title: "친절한 외교관",
     desc: "사람을 잘 챙기는 성격",
-    image: "images/result/esfj.png",
+    image: "./images/result/esfj.png",
   },
 
   ESFP: {
     mbti: "ESFP",
     title: "자유로운 연예인",
     desc: "즐거움과 에너지가 넘침",
-    image: "images/result/esfp.png",
+    image: "./images/result/esfp.png",
   },
 
   ISTJ: {
     mbti: "ISTJ",
     title: "청렴결백한 관리자",
     desc: "책임감이 매우 강함",
-    image: "images/result/istj.png",
+    image: "./images/result/istj.png",
   },
 
   ISTP: {
     mbti: "ISTP",
     title: "만능 재주꾼",
     desc: "문제 해결 능력이 뛰어남",
-    image: "images/result/istp.png",
+    image: "./images/result/istp.png",
   },
 
   ISFJ: {
     mbti: "ISFJ",
     title: "수호자",
     desc: "배려심 깊고 성실함",
-    image: "images/result/isfj.png",
+    image: "./images/result/isfj.png",
   },
 
   ISFP: {
     mbti: "ISFP",
     title: "예술가",
     desc: "감성적이고 자유로운 성격",
-    image: "images/result/isfp.png",
+    image: "./images/result/isfp.png",
   },
 };
 
